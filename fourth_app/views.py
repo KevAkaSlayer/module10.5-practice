5b9ca433c3d7182273429d8f539bbec61dc9de8d
KevAkaSlayer <pukman41@gmail.com>
2024-01-11 23:22:47 +0600

    geeks for geeks done

--- fourth_app/views.py
+++ fourth_app/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 import datetime
 # Create your views here.
 def home(request):
-    d = {'author':'kevin','age':16,'lst':['pycharm','quillbot','chatgpt'],'bdate':datetime.datetime.now(),'courses':[
+    d = {'author':'kevin','age':16,'value':['a', 'b', 'c', 'd'],
+         'lst':['pycharm','quillbot','chatgpt'],
+         'slash':"i'm kevin",
+         'val':'',
+         'valu':25,
+         'v':123456789,
+         'line':"one two three",
+         'upper':'AMIN',
+         'lower':'amin',
+         'timenow':datetime.datetime.now(),
+         'var':['states',['kanses',['lawrence','topeka'],'illinois']],
+         'main':[
+            {'name': 'zed', 'age': 19},
+            {'name': 'amy', 'age': 22},
+            {'name': 'joe', 'age': 31},
+        ],
+         'bdate':datetime.datetime.now(),
+         'courses':[
         {
             'id':1,
             'name':'python',
